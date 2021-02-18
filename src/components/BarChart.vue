c090900ccd06dd49ba78b25d123b17d8fb938f2e
derek1368 <kimmoonki90@naver.com>
2021-02-18 18:53:19 +0900

    practice of chart.js lib

---
+++ src/components/BarChart.vue
@@ -0,0 +1,36 @@
+<template>
+  <canvas ref="barChart" id="barChart"></canvas>
+</template>
+
+<script>
+import Chart from 'chart.js'
+
+export default {
+    props: ['propsdata'],
+
+mounted() {
+    var barChart = new Chart(this.$refs.barChart, {
+        type: 'bar',
+            data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: this.propsdata
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+  }
+
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/plugins/ChartPlugin.js
@@ -0,0 +1,13 @@
+import Chart from 'chart.js';
+
+export default {
+    insatll(Vue) {
+        Vue.prototype.$_Chart = Chart;
+
+        // BarChart.vue
+        // this.$_Chart
+        
+
+
+    }
+}
